c1f3acd33a832461a0aa9b91f42b43a2ce54d4d2
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-03-23 16:46:25 -0300

    NotasDetalhesActivity construída

--- app/src/main/java/projetos/danilo/mynotesmvvm/ui/detalhes/NotasDetalhesActivity.kt
+++ app/src/main/java/projetos/danilo/mynotesmvvm/ui/detalhes/NotasDetalhesActivity.kt
@@ -1,12 +1,35 @@
 package projetos.danilo.mynotesmvvm.ui.detalhes
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.include_toolbar.*
 import projetos.danilo.mynotesmvvm.R
 import projetos.danilo.mynotesmvvm.ui.base.BaseActivity
 
 class NotasDetalhesActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_notas_details)
+        setContentView(R.layout.activity_notas_details)
+
+        configurarToolbar(toolbarPrincipal, R.string.titulo_detalhe_notas)
+    }
+
+    /** objeto Nota para essa activity*/
+    companion object{
+        private const val EXTRA_TITULO = "EXTRA_TITULO"
+        private const val EXTRA_DESCRICAO = "EXTRA_DESCRICAO"
+        private const val EXTRA_COMENTARIO = "EXTRA_COMENTARIO"
+
+        fun getStartIntent(ctx: Context, titulo: String, descricao: String, comentario: String): Intent {
+            /**Intent(activity de origem, activity de destino*/
+            val intent = Intent(ctx, NotasDetalhesActivity::class.java)
+            intent.putExtra(EXTRA_TITULO, titulo)
+            intent.putExtra(EXTRA_DESCRICAO, descricao)
+            intent.putExtra(EXTRA_COMENTARIO, comentario)
+
+            return intent
+        }
+
     }
 }
